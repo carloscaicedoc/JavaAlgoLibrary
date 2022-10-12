dec85afa276d68aa628f611daa280019e0b203e8
Carlos Caicedo <calicheccmusica@gmail.com>
2022-10-12 12:35:35 -0700

    Java algorithm practice

--- Leetcode/Strings/Anagram/Test.java
+++ Leetcode/Strings/Anagram/Test.java
@@ -1,8 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         Solution test = new Solution();
-        String str1 = "karlos";
-        String str2 = "ceryuz";
+        String str1 = "study";
+        String str2 = "dusty";
         boolean output = test.isAnagram(str1, str2);
         System.out.println(output);
     }
