be2f37cf83b0c893667d950e41532745076c8385
Carlos Caicedo <calicheccmusica@gmail.com>
2022-10-12 12:31:07 -0700

    Java algorithm practice

---
+++ Leetcode/Strings/Anagram/Solution.java
@@ -0,0 +1,46 @@
+/* LeetCode 242. Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        // Early exit
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isAnagramSort(String s, String t) {
+        // Early exit
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        // Sorting
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        return Arrays.equals(str1, str2);
+    }
+}
---
+++ Leetcode/Strings/Anagram/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        String str1 = "karlos";
+        String str2 = "ceryuz";
+        boolean output = test.isAnagram(str1, str2);
+        System.out.println(output);
+    }
+}
