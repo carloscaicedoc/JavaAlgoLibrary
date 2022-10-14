ea8a8f0e29a9353818cbb56d98dcf0e91c78b440
Carlos Caicedo <calicheccmusica@gmail.com>
2022-10-14 13:56:17 -0700

    Java algorith practice - string comparion

--- Codewars/8kyu/PersonalizedMessage/Kata.java
+++ Codewars/8kyu/PersonalizedMessage/Kata.java
@@ -1,12 +1,20 @@
+/* Codewars 8kyu Grasshopper - Personalized Message
+
+Create a function that gives a personalized greeting. 
+This function takes two parameters: name and owner.
+Use conditionals to return the proper message:
+
+case : name equals owner - return 'Hello boss'
+case : otherwise         - return 'Hello guest'
+*/
 
 public class Kata {
-
+    // .equals method for String comparison
     public String greet(String name, String owner) {
         return name.equals(owner) ? "Hello boss" : "Hello guest";
     }
-    public String greet2(String name, String owner) {
 
+    public String greet2(String name, String owner) {
         return "Hello " + (name.equals(owner) ? "boss" : "guest");
     }
-
 }
