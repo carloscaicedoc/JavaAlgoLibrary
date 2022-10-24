61f8fd949ea20bc174fe01bdb722717364b61e14
Carlos Caicedo <calicheccmusica@gmail.com>
2022-10-24 12:40:50 -0700

    Java algorithm practice

--- Codewars/8kyu/SumMixedArray/TestSum.java
+++ Codewars/8kyu/SumMixedArray/TestSum.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class TestSum {
     public static void main(String[] args) {
         MixedSum test = new MixedSum();
+        
         int output = test.sumValues(Arrays.asList("5", 10, "15"));
         System.out.println(output);
     }
