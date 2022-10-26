9c7f392659e09ba0d6cd8892faf478f17ca06134
Carlos Caicedo <calicheccmusica@gmail.com>
2022-10-26 12:36:22 -0700

    Java algorithm practice

--- Codewars/8kyuy/Arrays/SumDoubleArray/SumArrayTest.java
+++ Codewars/8kyuy/Arrays/SumDoubleArray/SumArrayTest.java
@@ -6,7 +6,7 @@ public class SumArrayTest {
     @Test 
     public void testSum() {
         assertEquals(6, SumArray.sum(new double[] {1, 2, 3}), 1e-4);
-        assertEquals(9.2, SumArray.sum(new double[] {1, 5.2, 4, 0, -1}), 1e-4);
+        assertEquals(9.2, SumArray.sum(new double[] {1, 5.4, 4, 0, -1}), 1e-4);
         assertEquals(320, SumArray.sum(new double[] {30, 89, 100, 101}), 1e-4);
     }
 }
