b0104d8f6152f3da1f9e65dd33a739d71d4fc095
Carlos Caicedo <calicheccmusica@gmail.com>
2022-11-18 16:01:37 -0800

    Java algorithm practice

--- Codewars/7kyu/PrinterErrors/Printer.java
+++ Codewars/7kyu/PrinterErrors/Printer.java
@@ -84,13 +84,11 @@ public class Printer {
 
     public static String printerError8(String s) {
         int numErrors = 0;
-        
         for (int i = 0; i < s.length(); i++) {
           if(s.charAt(i) > 109){
             numErrors++;
           }
         }
-        
         return numErrors + "/" + s.length();
     }
 }
