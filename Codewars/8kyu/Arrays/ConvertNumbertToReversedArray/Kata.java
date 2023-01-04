550a4a7bf6d1a7d724cb1b8a1d7aaf3444d5ccdc
Carlos Caicedo <calicheccmusica@gmail.com>
2023-01-04 15:19:17 -0800

    Java algorithm practice

--- Codewars/8kyu/Arrays/ConvertNumbertToReversedArray/Kata.java
+++ Codewars/8kyu/Arrays/ConvertNumbertToReversedArray/Kata.java
@@ -2,13 +2,11 @@
 
 Given a random non-negative number, 
 return the digits of this number within an array in reverse order.
-
 Example:
 35231 => [1,3,2,5,3]
 */
 
 public class Kata {
-
     public static int[] digitize(long n) {
         String[] nums = new StringBuilder(String.valueOf(n)).reverse().toString().split("");
         int[] result = new int[nums.length];
@@ -16,9 +14,6 @@ public class Kata {
         for (int i = 0; i < nums.length; i++) {
             result[i] = Integer.parseInt(nums[i]);
         }
-
-        System.out.println(nums);
-
         return result;
     }
 }
