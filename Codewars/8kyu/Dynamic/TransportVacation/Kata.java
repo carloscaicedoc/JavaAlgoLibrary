9ee7b9bb23845becbb046040515c2d9cb5062496
Carlos Caicedo <calicheccmusica@gmail.com>
2023-01-06 13:42:24 -0800

    Java algorithm practice

---
+++ Codewars/8kyu/Dynamic/TransportVacation/Kata.java
@@ -0,0 +1,30 @@
+public class Kata {
+    public static int rentalCarCost(int d) {
+        int total = d * 40;
+        if (d >= 7) {
+            total -= 50;
+        } else if (d >= 3) {
+            total -= 20;
+        } 
+        return total;
+    }
+
+    public static int rentalCarCostTernary(int d) {
+        int total = d * 40;
+        return d < 3 ? total : d < 7 ? total - 20 : total - 50;
+    }
+
+    public static int rentalCarCostSwitch(int d) {
+        int total = d * 40;
+           
+        switch (d) {
+            case 0: case 1: case 2:
+            return total;
+
+            case 3: case 4: case 5: case 6:
+            return total - 20;
+
+            default: return total - 50;
+        }
+    }
+}
---
+++ Codewars/8kyu/Dynamic/TransportVacation/TestKata.java
@@ -0,0 +1,9 @@
+public class TestKata {
+    public static void main(String[] args) {
+        Kata kata = new Kata();
+
+        System.out.println(kata.rentalCarCost(7));
+        System.out.println(kata.rentalCarCostTernary(5));
+        System.out.println(kata.rentalCarCostSwitch(2));
+    }
+}
