8d3362d5195fbaf86fbdc32d42073ddac6c80aee
Carlos Caicedo <calicheccmusica@gmail.com>
2023-01-11 13:08:29 -0800

    Java algorithm practice

--- Codewars/8kyu/Arrays/ReduceButGrow/Kata.java
+++ Codewars/8kyu/Arrays/ReduceButGrow/Kata.java
@@ -1,6 +1,7 @@
 /* Codewars 8kyu Reduce but Grow
 
-Given a non-empty array of integers, return the result of multiplying the values together in order. 
+Given a non-empty array of integers, 
+return the result of multiplying the values together in order. 
 Example:
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 */
